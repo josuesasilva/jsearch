b9b3e6879dcd9ad0f6d557f194621a3262c71158
Josue Santos <josuesasilva@gmail.com>
2016-09-11 22:06:17 -0300

    Add BM25 similarity feature.

--- src/br/pucminas/ri/jsearch/Main.java
+++ src/br/pucminas/ri/jsearch/Main.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Scanner;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.IndexWriter;
@@ -19,37 +22,158 @@ import org.apache.lucene.store.FSDirectory;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        Date start = new Date();
-        String userQuery = new String();
+    private static String[] getUserParam(String[] args) {
+        String[] param = new String[2];
 
         if (args.length > 1 && args[0].equals("-q")) {
             if (args.length > 2) {
                 for (int i = 1; i < args.length; i++) {
                     if (i == args.length - 2) {
-                        userQuery += args[i];
+                        param[0] += args[i];
                     } else {
-                        userQuery = userQuery + " " + args[i] + " ";
+                        param[0] = param[0] + " " + args[i] + " ";
                     }
                 }
             } else {
-                userQuery = args[1];
+                param[0] = args[1];
             }
+        } else if (args.length > 1 && args[0].equals("-f")) {
+            param[0] = "";
+            param[1] = args[1];
+        } else if (args.length == 0) {
+            param[0] = "test";
+            param[1] = "";
+        } else {
+            System.out.println("error: unsupported option");
+            System.out.print("\nUsage:");
+            System.out.print("\njsearch.jar [option]");
+            System.out.print("\n-q userquery");
+            System.out.print("\n-f file with queries\n");
+            System.exit(0);
         }
 
+        return param;
+    }
+
+    private static void performIndexer(File docDir, Directory dir, IndexWriterConfig iwc) throws IOException {
+        System.out.println("Indexing to directory '" + Constants.INDEX_PATH + "'...");
+
+        Date start = new Date();
+
+        try (IndexWriter writer = new IndexWriter(dir, iwc)) {
+            Indexer.indexDocs(writer, docDir);
+        }
+
+        Date end = new Date();
+        System.out.println("\nIndex time: " + (end.getTime() - start.getTime()
+                + " milliseconds\n"));
+    }
+
+    private static void performUserQuery(String userQuery) {
+        System.out.println("Query: " + userQuery);
+
+        Date start = new Date();
+        Searcher.search("0", userQuery, RankingEnum.BM25, null);
+        Date end = new Date();
+        System.out.println("\nSearch time :" + (end.getTime() - start.getTime()
+                + " total milliseconds"));
+
+        start = new Date();
+        Searcher.search("0", userQuery, RankingEnum.ROCCHIO, null);
+        end = new Date();
+        System.out.println("\nSearch time :" + (end.getTime() - start.getTime()
+                + " total milliseconds"));
+    }
+
+    private static HashMap<String, String> getQueriesFromFile(String queriesFile) {
+        File file = new File(queriesFile);
+
+        if (file.isFile()) {
+            Scanner in = null;
+            HashMap<String, String> queries = new HashMap();
+
+            try {
+                in = new Scanner(file);
+            } catch (IOException e) {
+                System.err.println(Arrays.toString(e.getStackTrace()));
+            }
+
+            while (in != null && in.hasNextLine()) {
+
+                StringBuilder qid = new StringBuilder();
+                StringBuilder query = new StringBuilder();
+                String line = in.nextLine();
+
+                if (line.contains("|")) {
+                    boolean flag = false;
+                    for (char c : line.toCharArray()) {
+                        if (c == '|') {
+                            flag = true;
+                            continue;
+                        }
+
+                        if (!flag) {
+                            qid.append(c);
+                        } else {
+                            query.append(c);
+                        }
+                    }
+                } else {
+                    boolean flag = false;
+                    for (char c : line.toCharArray()) {
+                        if (c == ' ') {
+                            flag = true;
+                        }
+
+                        if (!flag) {
+                            qid.append(c);
+                        } else {
+                            query.append(c);
+                        }
+                    }
+                }
+
+                queries.put(qid.toString(), query.toString());
+            }
+
+            if (in != null) {
+                in.close();
+            }
+
+            return queries;
+        }
+
+        return new HashMap<>();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //
+        // Begin - Parse user query
+        //
+        String userQuery = getUserParam(args)[0];
+        String queriesFile = getUserParam(args)[1];
+        //
+        // End - Parse user query
+        //
+        //
+        // Begin - Get documents to perform indexer
+        //
         final File docDir = new File(Constants.DOCS_PATH);
         if (!docDir.exists() || !docDir.canRead()) {
             System.out.println("Document directory '" + docDir.getAbsolutePath() + "' does not exist or is not readable, please check the path");
             System.exit(1);
         }
+        //
+        // End - Get documents to perform indexer
+        //
 
         try {
-            System.out.println("Indexing to directory '" + Constants.INDEX_PATH + "'...");
-
+            //
+            // Begin - Setup
+            //
             Path path = Paths.get(Constants.INDEX_PATH);
             Directory dir = FSDirectory.open(path);
             Analyzer analyzer = new StandardAnalyzer();
@@ -57,23 +181,32 @@ public class Main {
 
             iwc.setOpenMode(OpenMode.CREATE);
             iwc.setRAMBufferSizeMB(512.0);
+            //
+            // End - Setup
+            //
 
-            try (IndexWriter writer = new IndexWriter(dir, iwc)) {
-                Indexer.indexDocs(writer, docDir);
+            //
+            // Begin - Perform Index
+            //
+            performIndexer(docDir, dir, iwc);
+            //
+            // End - Perform Index
+            //
+            //
+            // Begin - Perform Search
+            //
+            if (!userQuery.isEmpty()) {
+                performUserQuery(userQuery);
+            } else if (!queriesFile.isEmpty()) {
+                HashMap<String, String> queries = getQueriesFromFile(queriesFile);
+                Searcher.search(queries, RankingEnum.BM25);
+                //Searcher.search(queries, RankingEnum.ROCCHIO);
+            } else {
+                System.exit(1);
             }
-
-            Date end = new Date();
-            System.out.println("\nIndex time :" + (end.getTime() - start.getTime()
-                    + " total milliseconds\n"));
-
-            userQuery = userQuery.isEmpty() ? "test" : userQuery;
-            System.out.println("Query: " + userQuery);
-            start = new Date();
-            Searcher.search(userQuery);
-            end = new Date();
-
-            System.out.println("\nSearch time :" + (end.getTime() - start.getTime()
-                    + " total milliseconds"));
+            //
+            // End - Perform Search
+            //
 
         } catch (IOException e) {
             System.out.println(e.getMessage());
--- src/br/pucminas/ri/jsearch/Searcher.java
+++ src/br/pucminas/ri/jsearch/Searcher.java
@@ -1,9 +1,14 @@
 package br.pucminas.ri.jsearch;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
@@ -16,6 +21,7 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.similarities.BM25Similarity;
 
 /**
  *
@@ -23,20 +29,88 @@ import org.apache.lucene.queryparser.classic.QueryParser;
  */
 public class Searcher {
 
-    public static void search(String text) {
+    public static void search(HashMap<String, String> queries, RankingEnum ranking) throws FileNotFoundException {
+        String tag = "default";
+
+        switch (ranking) {
+            case ROCCHIO:
+                tag = "Rocchio";
+                break;
+            case BM25:
+                tag = "BM25";
+                break;
+            default:
+                break;
+        }
+
+        File outDir = new File(Constants.RESULT_PATH);
+        outDir.mkdir();
+        String resultsFile = String.format("%s/%s.out",
+                Constants.RESULT_PATH, tag);
+        File outFile = new File(resultsFile);
+        
+        System.out.printf("Writing query results to '%s'...\n", resultsFile);
+        
+        Date start = new Date();
+        
+        try (PrintWriter writer = new PrintWriter(outFile)) {
+            queries.keySet().stream().forEach((key) -> {
+                String query = queries.get(key);
+                search(key, query, ranking, writer);
+            });    
+            writer.close();
+        }
+        
+        Date end = new Date();
+        
+        System.out.println("\nQuery average time: " + 
+                ((end.getTime() - start.getTime())/queries.keySet().size()
+                + " milliseconds\n"));
+        
+        System.out.println("\nDone.");
+    }
+
+    public static void search(String qid, String text, RankingEnum ranking,
+            PrintWriter writer) {
         try {
             Path path = Paths.get(Constants.INDEX_PATH);
             Directory directory = FSDirectory.open(path);
             IndexReader indexReader = DirectoryReader.open(directory);
             IndexSearcher indexSearcher = new IndexSearcher(indexReader);
             QueryParser queryParser = new QueryParser("contents", new StandardAnalyzer());
+            String rankingName = new String();
+
             Query query = queryParser.parse(text);
-            TopDocs topDocs = indexSearcher.search(query, 10);
 
-            System.out.println("totalHits " + topDocs.totalHits);
+            switch (ranking) {
+                case ROCCHIO:
+                    rankingName = "Rocchio";
+                    break;
+                case BM25:
+                    rankingName = "BM25";
+                    indexSearcher.setSimilarity(new BM25Similarity(1.2f, 0.75f));
+                    break;
+                default:
+                    break;
+            }
 
+            TopDocs topDocs = indexSearcher.search(query, Constants.MAX_SEARCH);
+            ScoreDoc[] hits = topDocs.scoreDocs;
+
+            int i = 0;
             for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
                 Document document = indexSearcher.doc(scoreDoc.doc);
+
+                String out = String.format("%s %s %s %d %f %s", qid, "Q0",
+                        document.get("docno"), i, hits[i].score, rankingName);
+
+                if (writer != null) {
+                    writer.println(out);
+                } else {
+                    System.out.println(out);
+                }
+
+                i++;
             }
 
         } catch (IOException | ParseException e) {
