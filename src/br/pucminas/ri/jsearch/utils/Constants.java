385ebf48a9ff452d18fc9ab6a268284f79779409
Josue Santos <josuesasilva@gmail.com>
2016-10-09 11:23:12 -0300

    Refactoring.

---
+++ src/br/pucminas/ri/jsearch/utils/Constants.java
@@ -0,0 +1,19 @@
+
+package br.pucminas.ri.jsearch.utils;
+
+/**
+ *
+ * @author josue
+ */
+public class Constants {
+    public static final String INDEX_PATH = "index";
+    public static String DOCS_PATH = "data";
+    public static String RESULT_PATH = "release";    
+    public static int MAX_SEARCH = 10;
+    public static final String FILE_NAME = "filename";
+    public static final String FILE_PATH = "filepath";
+    public static final String FILE_CONTENT = "filecontent";
+    public static final String DOC_CONTENT = "contents";
+    public static final String DOC_TITLE = "docno";
+    public static final String WORDNET_DATA = "wn_s.pl";
+}
