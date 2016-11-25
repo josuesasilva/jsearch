004eaaccbf564052726283092945525cd0fe2b3e
Josue Santos <josuesasilva@gmail.com>
2016-11-24 23:13:41 -0200

    update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-# # MAP e P BM25
-# ./release/trec_eval release/en.qrels release/BM25.out > result/output.txt > result/output.txt
-# echo "\n---\n" >> result/output.txt
+# MAP e P BM25
+./release/trec_eval release/en.qrels release/BM25.out > result/output.txt
+echo "\n---\n" >> result/output.txt
 
 # # NDCG BM25
 # echo "ndcg BM25" >> result/output.txt
@@ -18,9 +18,9 @@
 # ./release/trec_eval -q -c -m ndcg.0=0,1=1,2=3 release/en.qrels release/Rocchio.out >> result/output.txt
 # echo "\n---\n" >> result/output.txt
 
-# # MAP e P QueryExpansion
-# ./release/trec_eval release/en.qrels release/QueryExpansion.out >> result/output.txt
-# echo "\n---\n" >> result/output.txt
+# MAP e P QueryExpansion
+./release/trec_eval release/en.qrels release/QueryExpansion.out >> result/output.txt
+echo "\n---\n" >> result/output.txt
 
 # # NDCG QueryExpansion
 # echo "ndcg QueryExpansion" >> result/output.txt
@@ -29,15 +29,15 @@
 
 
 
-echo "ndcg BM25" >> result/output.txt
-./release/trec_eval -q release/en.qrels release/BM25.out > result/output-query.txt
-./release/trec_eval -q -c -m ndcg.0=0,1=1,2=3 release/en.qrels release/BM25.out >> result/output-query.txt
+# echo "ndcg BM25" >> result/output.txt
+# ./release/trec_eval -q release/en.qrels release/BM25.out > result/output-query.txt
+# ./release/trec_eval -q -c -m ndcg.0=0,1=1,2=3 release/en.qrels release/BM25.out >> result/output-query.txt
 
 
-echo "ndcg Rocchio" >> result/output.txt
-./release/trec_eval -q release/en.qrels release/Rocchio.out >> result/output-query.txt
-./release/trec_eval -q -c -m ndcg.0=0,1=1,2=3 release/en.qrels release/Rocchio.out >> result/output-query.txt
+# echo "ndcg Rocchio" >> result/output.txt
+# ./release/trec_eval -q release/en.qrels release/Rocchio.out >> result/output-query.txt
+# ./release/trec_eval -q -c -m ndcg.0=0,1=1,2=3 release/en.qrels release/Rocchio.out >> result/output-query.txt
 
-echo "ndcg QueryExpansion" >> result/output-query.txt
-./release/trec_eval -q release/en.qrels release/QueryExpansion.out >> result/output-query.txt
-./release/trec_eval -q -c -m ndcg.0=0,1=1,2=3 release/en.qrels release/QueryExpansion.out >> result/output-query.txt
+# echo "ndcg QueryExpansion" >> result/output-query.txt
+# ./release/trec_eval -q release/en.qrels release/QueryExpansion.out >> result/output-query.txt
+# ./release/trec_eval -q -c -m ndcg.0=0,1=1,2=3 release/en.qrels release/QueryExpansion.out >> result/output-query.txt
